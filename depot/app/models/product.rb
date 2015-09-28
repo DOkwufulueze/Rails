a18534acb028869b1bde85e6ac766583eb7883de
Daniel Okwufulueze <daniel@dealdey.com>
2015-09-28 17:01:02 +0100

    Validation!

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,9 +1,13 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: ':::Please supply a GIF, JPG or PNG Image URL'
+  }
+  validates :title, length: {
+    minimum: 10,
+    message: ':::Title should be 10 or more characters long'
   }
 end
